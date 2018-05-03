86a2f17876334c25adec308980e0f107b84e9c84
Anderson Keiji Matsuki Ito <anderson.ito@yandeh.com.br>
2018-05-03 16:37:30 -0300

    using spark session

--- src/main/scala/org/sharpsw/spark/TextFileLoader.scala
+++ src/main/scala/org/sharpsw/spark/TextFileLoader.scala
@@ -1,12 +1,12 @@
 package org.sharpsw.spark
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 
 object TextFileLoader {
-  val sc:SparkContext = new SparkContext(new SparkConf().setAppName("TextFileLoader").setMaster("local[*]"))
+  val sparkSession: SparkSession = SparkSession.builder.master("local[*]").appName("TextFileLoader").getOrCreate()
 
   def main(args: Array[String]): Unit = {
-    val fileContents = sc.textFile(args(0))
+    val fileContents = sparkSession.sparkContext.textFile(args(0))
     println(fileContents)
   }
 }
