17221398ab3c28dacde85ebe64e8954563d7ebdd
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2019-11-18 21:48:33 +0100

    added partition number

--- src/main/scala/org/sharpsw/spark/ExtractLondonCrimeData.scala
+++ src/main/scala/org/sharpsw/spark/ExtractLondonCrimeData.scala
@@ -17,10 +17,11 @@ object ExtractLondonCrimeData {
   private val S3DestinationPrefix:String = "--s3-dest-prefix"
   private val Destination:String = "--destination"
   private val Master: String = "--master"
+  private val PartitionNumber: String = "--partition-number"
 
   private val ZipFileName: String = "london-crime.zip"
 
-  @transient lazy val logger = Logger.getLogger(getClass.getName)
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
   Logger.getLogger("org.apache").setLevel(Level.ERROR)
 
   def main(args: Array[String]): Unit = {
@@ -52,6 +53,8 @@ object ExtractLondonCrimeData {
 
       val destinationFolder = argsMap(Destination)
 
+      val partitionNumber: Int = argsMap.getOrElse(PartitionNumber, "1").toInt
+
       logger.info(s"Reading $inputFile contents")
       val fileContents = sparkSession.sparkContext.textFile(inputFile)
       val (headerColumns, contents) = timed("Reading file contents", readContents(fileContents, sparkSession))
@@ -62,81 +65,81 @@ object ExtractLondonCrimeData {
 
       logger.info("Extracting distinct boroughs")
       val boroughs = timed("Extracting distinct boroughs", extractDistinctBoroughs(contents))
-      timed("Exporting boroughs to csv", saveDataFrameToCsv(boroughs, 1, buildFilePath(destinationFolder, "borough_csv")))
+      timed("Exporting boroughs to csv", saveDataFrameToCsv(boroughs, partitionNumber, buildFilePath(destinationFolder, "borough_csv")))
       timed("Exporting the boroughs data frame to parquet", saveDataFrameToParquet(boroughs, buildFilePath(destinationFolder, "bourough_parquet")))
 
       logger.info("Extracting LSOA codes by borough")
       val lsoa = timed("Extracting LSOA codes by borough", extractBoroughLsoa(contents))
-      timed("Exporting lsoa codes to csv", saveDataFrameToCsv(lsoa, 1, buildFilePath(destinationFolder, "lsoa_csv")))
+      timed("Exporting lsoa codes to csv", saveDataFrameToCsv(lsoa, partitionNumber, buildFilePath(destinationFolder, "lsoa_csv")))
       timed("Exporting lsoa codes to parquet", saveDataFrameToParquet(lsoa, buildFilePath(destinationFolder, "lsoa_parquet")))
 
       logger.info("Extracting distinct major crime categories")
       val majorCrimeCategories = timed("Extracting major categories", extractDistinctMajorCrimeCategories(contents))
-      timed("Exporting major categories to csv", saveDataFrameToCsv(majorCrimeCategories, 1, buildFilePath(destinationFolder, "major_category_csv")))
+      timed("Exporting major categories to csv", saveDataFrameToCsv(majorCrimeCategories, partitionNumber, buildFilePath(destinationFolder, "major_category_csv")))
       timed("Exporting major categories to parquet", saveDataFrameToParquet(majorCrimeCategories, buildFilePath(destinationFolder, "major_category_parquet")))
 
       logger.info("Extracting distinct minor crime categories")
       val minorCrimeCategories = timed("Extracting minor categories", extractDistinctMinorCrimeCategories(contents))
-      timed("Exporting minor category to csv", saveDataFrameToCsv(minorCrimeCategories, 1, buildFilePath(destinationFolder, "minor_category_csv")))
+      timed("Exporting minor category to csv", saveDataFrameToCsv(minorCrimeCategories, partitionNumber, buildFilePath(destinationFolder, "minor_category_csv")))
       timed("Exporting minor category to parquet", saveDataFrameToParquet(minorCrimeCategories, buildFilePath(destinationFolder, "minor_category_parquet")))
 
       logger.info("Extracting distinct combined crime categories")
       val categories = timed("Extracting categories to csv", extractCombinedCategories(contents))
-      timed("Exporting categories to csv", saveDataFrameToCsv(categories, 1, buildFilePath(destinationFolder, "categories_csv")))
+      timed("Exporting categories to csv", saveDataFrameToCsv(categories, partitionNumber, buildFilePath(destinationFolder, "categories_csv")))
       timed("Exporting categories to parquet", saveDataFrameToParquet(categories, buildFilePath(destinationFolder, "categories_parquet")))
 
       logger.info("Calculating total crimes by borough")
       val crimesByBorough = timed("Calculate total crimes by borough", calculateTotalCrimeCountByBorough(contents))
-      timed("Exporting resulting aggregation to CSV", saveDataFrameToCsv(crimesByBorough, 1, buildFilePath(destinationFolder, "total_crimes_by_borough_csv")))
+      timed("Exporting resulting aggregation to CSV", saveDataFrameToCsv(crimesByBorough, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_borough_csv")))
       timed("Exporting resulting aggregation to parquet", saveDataFrameToParquet(crimesByBorough, buildFilePath(destinationFolder, "total_crimes_by_borough_parquet")))
 
       logger.info("Calculating total crimes by major category")
       val crimesByMajorCategory = timed("Calculate total crimes by major category", calculateCrimesByMajorCategory(contents))
-      timed("Exporting resulting aggregation - by major category to csv", saveDataFrameToCsv(crimesByMajorCategory, 1, buildFilePath(destinationFolder, "total_crimes_by_major_category_csv")))
+      timed("Exporting resulting aggregation - by major category to csv", saveDataFrameToCsv(crimesByMajorCategory, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_major_category_csv")))
       timed("Exporting resulting aggregation - by major category to parquet", saveDataFrameToParquet(crimesByMajorCategory, buildFilePath(destinationFolder, "total_crimes_by_major_category_parquet")))
 
       logger.info("Calculating total crimes by minor category")
       val crimesByMinorCategory = timed("Calculate total crimes by minor category", calculateCrimeCountByMinorCategory(contents))
-      timed("Exporting resulting aggregation - by minor category to csv", saveDataFrameToCsv(crimesByMinorCategory, 1, buildFilePath(destinationFolder, "total_crimes_by_minor_category_csv")))
+      timed("Exporting resulting aggregation - by minor category to csv", saveDataFrameToCsv(crimesByMinorCategory, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_minor_category_csv")))
       timed("Exporting resulting aggregation - by minor category to parquet", saveDataFrameToParquet(crimesByMinorCategory, buildFilePath(destinationFolder, "total_crimes_by_minor_category_parquet")))
 
       logger.info("Calculating total crimes by borough and year")
       val crimesByBoroughAndYear = timed("Calculate total crimes by borough and year", calculateCrimeCountByBoroughAndYear(contents))
-      timed("Exporting resulting aggregation - by borough and year to csv", saveDataFrameToCsv(crimesByBoroughAndYear, 1, buildFilePath(destinationFolder, "total_crimes_by_borough_year_csv")))
+      timed("Exporting resulting aggregation - by borough and year to csv", saveDataFrameToCsv(crimesByBoroughAndYear, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_borough_year_csv")))
       timed("Exporting resulting aggregation - by borough and year to parquet", saveDataFrameToParquet(crimesByBoroughAndYear, buildFilePath(destinationFolder, "total_crimes_by_borough_year_parquet")))
 
       logger.info("Calculating total crimes by major category and year")
       val crimesByMajorCategoryAndYear = timed("Calculate total crimes by major category and year", calculateCrimesByMajorCategoryAndYear(contents))
-      timed("Exporting resulting aggregation - by major category and year to csv", saveDataFrameToCsv(crimesByMajorCategoryAndYear, 1, buildFilePath(destinationFolder, "total_crimes_by_major_category_year_csv")))
+      timed("Exporting resulting aggregation - by major category and year to csv", saveDataFrameToCsv(crimesByMajorCategoryAndYear, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_major_category_year_csv")))
       timed("Exporting resulting aggregation - by major category and year to parquet", saveDataFrameToParquet(crimesByMajorCategoryAndYear, buildFilePath(destinationFolder, "total_crimes_by_major_category_year_parquet")))
 
       logger.info("Calculating total crimes by minor category and year")
       val crimesByMinorCategoryAndYear = timed("Calculate total crimes by minor category and year", calculateCrimesByMinorCategoryAndYear(contents))
-      timed("Exporting resulting aggregation - by minor category and year to csv", saveDataFrameToCsv(crimesByMinorCategoryAndYear, 1, buildFilePath(destinationFolder, "total_crimes_by_minor_category_year_csv")))
+      timed("Exporting resulting aggregation - by minor category and year to csv", saveDataFrameToCsv(crimesByMinorCategoryAndYear, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_minor_category_year_csv")))
       timed("Exporting resulting aggregation - by minor category and year to parquet", saveDataFrameToParquet(crimesByMinorCategoryAndYear, buildFilePath(destinationFolder, "total_crimes_by_minor_category_year_parquet")))
 
       logger.info("Calculating total crimes by year")
       val crimesByYear = timed("Calculate total crimes by year", calculateCrimesByYear(contents))
-      timed("Exporting crimes by year results to csv", saveDataFrameToCsv(crimesByYear, 1, buildFilePath(destinationFolder, "total_crimes_by_year_csv")))
+      timed("Exporting crimes by year results to csv", saveDataFrameToCsv(crimesByYear, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_year_csv")))
       timed("Exporting crimes by year results to parquet", saveDataFrameToParquet(crimesByYear, buildFilePath(destinationFolder, "total_crimes_by_year_parquet")))
 
       logger.info("Calculating total crimes by year and month")
       val crimesByYearMonth = timed("Calculate total crimes by year", calculateCrimesByYearAndMonth(contents))
-      timed("Exporting crimes by year and month results to CSV", saveDataFrameToCsv(crimesByYearMonth, 1, buildFilePath(destinationFolder, "total_crimes_by_year_month_csv")))
+      timed("Exporting crimes by year and month results to CSV", saveDataFrameToCsv(crimesByYearMonth, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_year_month_csv")))
       timed("Exporting crimes by year and month results to parquet", saveDataFrameToParquet(crimesByYearMonth, buildFilePath(destinationFolder, "total_crimes_by_year_month_parquet")))
 
       logger.info("Percentages of crimes by years")
       val crimePercentageByYear = calculateCrimesPercentageByCategoryAndYear(contents, sparkSession)
       crimePercentageByYear.foreach(item => {
         logger.info(s"Exporting crime percentage for year '${item._1}'")
-        timed(s"Exporting crime percentage in ${item._1} to CSV", saveDataFrameToCsv(item._2, 1, buildFilePath(destinationFolder, s"crime_percentage_${item._1}_csv")))
+        timed(s"Exporting crime percentage in ${item._1} to CSV", saveDataFrameToCsv(item._2, partitionNumber, buildFilePath(destinationFolder, s"crime_percentage_${item._1}_csv")))
         timed(s"Exporting crime percentage in ${item._1} to parquet", saveDataFrameToParquet(item._2, buildFilePath(destinationFolder, s"crime_percentage_${item._1}_parquet")))
       })
 
       logger.info("Calculating total crimes by year and LSOA codes")
       val totalCrimesByYearAndLsoa = timed("Calculating total crimes by year and lsoa codes", calculateTotalCrimesByYearLsoaCode(contents))
-      timed("Exporting results to csv", saveDataFrameToCsv(totalCrimesByYearAndLsoa, 1, buildFilePath(destinationFolder, "total_crimes_by_year_lsoa_code_csv")))
-      timed("Exporting results to parquet", saveDataFrameToCsv(totalCrimesByYearAndLsoa, 1, buildFilePath(destinationFolder, "total_crimes_by_year_lsoa_code_parquet")))
+      timed("Exporting results to csv", saveDataFrameToCsv(totalCrimesByYearAndLsoa, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_year_lsoa_code_csv")))
+      timed("Exporting results to parquet", saveDataFrameToCsv(totalCrimesByYearAndLsoa, partitionNumber, buildFilePath(destinationFolder, "total_crimes_by_year_lsoa_code_parquet")))
       println(timing)
 
       if(argsMap.contains(S3DestinationBucket)) {
